5d58faa5b71b72469914ea49005b4ab5bfe961df
김학현 <gimhaghyeon@gimhaghyeon-ui-MacBookPro.local>
2018-10-31 15:26:19 +0900

    commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res, next) => {
-    res.send('hello world!');
+    res.send('hello world welcome!');
 });
 
 app.listen(port, () => {
